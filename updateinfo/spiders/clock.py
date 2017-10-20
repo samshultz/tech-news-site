7a3aa5fc118e7a2a7fc17d097a520ffb526e6409
Samuel Taiwo <taiwogabrielsamuel@gmail.com>
2017-10-20 18:23:32 +0100

    made it better

--- updateinfo/spiders/clock.py
+++ updateinfo/spiders/clock.py
@@ -1,17 +1,17 @@
-from . import future
-from . import hacker
-from . import techcrunch
+import future
+import hacker
+import techcrunch
 from apscheduler.schedulers.blocking import BlockingScheduler
 from scrapy.crawler import CrawlerProcess
 
 
-
 sched = BlockingScheduler()
+
 
 @sched.scheduled_job('interval', minutes=60)
 def timed_job():
     process = CrawlerProcess({
-    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
     })
     process.crawl(future.BBCFutureSpider)
     process.crawl(hacker.HackerNewsSpider)
