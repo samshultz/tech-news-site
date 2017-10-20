70c18d86c398c61672c557866e39b1d27d6d712f
Samuel Taiwo <taiwogabrielsamuel@gmail.com>
2017-10-20 18:06:41 +0100

    added a scheduler

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn infoupdate.wsgi
+clock: python clock.py
---
+++ updateinfo/spiders/clock.py
@@ -0,0 +1,21 @@
+from . import future
+from . import hacker
+from . import techcrunch
+from apscheduler.schedulers.blocking import BlockingScheduler
+from scrapy.crawler import CrawlerProcess
+
+
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=60)
+def timed_job():
+    process = CrawlerProcess({
+    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+    })
+    process.crawl(future.BBCFutureSpider)
+    process.crawl(hacker.HackerNewsSpider)
+    process.crawl(techcrunch.TechCrunchSpider)
+    process.start()
+
+sched.start()
